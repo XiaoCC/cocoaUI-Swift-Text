45cc3fd93417d2ce74651d162cbca4dec4346dbe
XiaoCC <284617787@qq.com>
2015-11-10 22:37:02 +0800

    cocoaUI-Swift-Text
    cocoaUI-Swift-Text

---
+++ addClassText/IKit/include/IKit/IKit.h
@@ -0,0 +1,25 @@
+/*
+ Copyright (c) 2014 ideawu. All rights reserved.
+ Use of this source code is governed by a license that can be
+ found in the LICENSE file.
+ 
+ @author:  ideawu
+ @website: http://www.cocoaui.com/
+ */
+
+#import <UIKit/UIKit.h>
+
+#import "IKitUtil.h"
+#import "IStyle.h"
+#import "IStyleUtil.h"
+#import "IView.h"
+#import "ITable.h"
+#import "ITableRow.h"
+#import "ILabel.h"
+#import "IInput.h"
+#import "IButton.h"
+#import "ISwitch.h"
+#import "IImage.h"
+#import "IPullRefresh.h"
+#import "IRefreshControl.h"
+#import "IPopover.h"
--- addClassText/ViewController.swift
+++ addClassText/ViewController.swift
@@ -8,16 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: ITable {
+    
+    let boxView = IView.namedView("addClassText")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //CocoaUI布局
+        self.addIViewRow(self.boxView)
+        self.reload()
+
+        let zfb = self.boxView.getViewById("zfb") as IView
+        let wxzf = self.boxView.getViewById("wxzf") as IView
+        let zfb1 = self.boxView.getViewById("zfb1") as IView
+        let wxzf1 = self.boxView.getViewById("wx1") as IView
+        let qdzf = self.boxView.getViewById("qdzf") as! IButton
+        
+        zfb1.style.addClass("off")
+        wxzf1.style.addClass("off")
+        
+        //事件
+        zfb.addEvent(IEventClick, handler: { (_, _) -> Void in
+            //支付宝
+            //style.style.set 测试
+//            zfb1.style.set("background:url(on@2x.png) no-repeat;")
+//            wxzf1.style.set("background:url(off@2x.png) no-repeat;")
+            
+            //style.addClass测试
+            zfb1.style.addClass("on")
+            wxzf1.style.addClass("off")
+        })
+        wxzf.addEvent(IEventClick, handler: { (_, _) -> Void in
+            //微信
+            //style.set 测试
+//            zfb1.style.set("background:url(off@2x.png) no-repeat;")
+//            wxzf1.style.set("background:url(on@2x.png) no-repeat;")
+            
+            //style.addClass测试
+            zfb1.style.addClass("off")
+            wxzf1.style.addClass("on")
+        })
+        qdzf.addEvent(IEventClick, handler: { (_, _) -> Void in
+            //确认支付
+        })
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
